8c3ac74e2f706a0ed5aa92061c7aaa8e68a0da66
Desmond95 <74813903+Desmond95@users.noreply.github.com>
2021-02-17 14:49:18 +0100

    inputfield .dart modification

---
+++ lib/inputfield.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  InputField({
+    @required this.prefixIcon,
+    this.suffixIcon,
+    @required this.hint,
+    @required this.labelText,
+    @required this.passwordhidden,
+    this.maxTextLength,
+    this.onPressed,
+    this.suffixIcon2,
+    this.secureText,
+  });
+
+  final IconData prefixIcon, suffixIcon, suffixIcon2;
+  final String hint;
+  final String labelText;
+  final bool passwordhidden;
+  final int maxTextLength;
+  final Function onPressed;
+
+  bool secureText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: labelText,
+        focusColor: Color(0xFF0B0F82),
+        labelStyle: TextStyle(
+            // color: Color(0xFF0B0F82),
+            color: Colors.grey.shade400,
+            fontWeight: FontWeight.bold,
+            fontSize: 17),
+        hintText: hint,
+        prefixIcon: Icon(
+          prefixIcon,
+        ),
+        suffixIcon: IconButton(
+          icon: Icon(secureText ? Icons.visibility : Icons.visibility_off),
+          onPressed: onPressed,
+        ),
+        border: OutlineInputBorder(),
+      ),
+      keyboardType: TextInputType.emailAddress,
+      obscureText: passwordhidden,
+      maxLength: maxTextLength,
+    );
+  }
+}
